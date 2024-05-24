b8bc447f894c2a1f817d222666f9238cca5c3ccb
Klauss Ratheg Werneck <rathgeklauss@gmail.com>
2024-05-24 17:12:25 -0300

    commit1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> vida </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componente/Cabecalho/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css';
+
+function Nav() {
+    return(
+        <div className="nav">
+            <h1>Cabecalho</h1>
+        </div>
+    );
+
+}
+
+export default Nav;
